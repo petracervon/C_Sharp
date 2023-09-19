517b2e92108942fbc1c806e0ec6a7589d3927a57
Peter <renemebel@yandex.ru>
2023-09-19 15:52:02 +0300

    работа с приветсвием

---
+++ Examples5/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Write("Введите имя ");
+string username = Console.ReadLine();
+if (username == "Маша")
+{Console.WriteLine("Ура, это маша!");}
+else 
+{
+Console.Write("Привет, ");
+Console.Write(username);
+}
+
